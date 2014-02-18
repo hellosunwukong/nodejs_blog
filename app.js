5d73c81648d0e1d6f7cb803dd68da7ff2700cddd
Flex_ <234134398@qq.com>
2014-02-19 02:02:11 +0800

    no message

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var config=require('./config');
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('port', config.web.port || 3000);
+app.set('views', path.join(config.dir.viewdir, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
